bf4b9484dc3144deff12c66b86b51f4225f8ab64
Jirayu Sirivorawong <jirayu.work@gmail.com>
2022-12-02 12:12:10 -0500

    server delete list and task

--- TaskRabbit/src/backend/server.py
+++ TaskRabbit/src/backend/server.py
@@ -145,6 +145,37 @@ def edit_task_url():
         'status': 0
     })
 
+@ app.route("/deleteList", methods=['POST'])
+def delete_list_url():
+    content = request.json
+    user_id = content['userId']
+    list_id = content['listId']
+
+    # check user id must be integer
+    if not user_id or not isinstance(user_id, int):
+        raise InvalidAPIUsage("Invalid userId")
+
+    delete_list(list_id)
+    return jsonify({
+        'status': 0
+    })
+
+@ app.route("/deleteTask", methods=['POST'])
+def delete_task_url():
+    content = request.json
+    user_id = content['userId']
+    task_id = content['taskId']
+
+    # check user id must be integer
+    if not user_id or not isinstance(user_id, int):
+        raise InvalidAPIUsage("Invalid userId")
+
+    delete_task(task_id)
+    return jsonify({
+        'status': 0
+    })
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
